ede2fb5c899319f6c437f96e62754746fd4cf63e
Mariano <marianoesfer@gmail.com>
2024-11-07 23:06:42 +0100

    prueba

---
+++ public/javascripts/front.js
@@ -0,0 +1,30 @@
+console.log("Hola mundo...");
+
+document.querySelector("input#lenguaje").value = navigator.userAgent;
+
+document.querySelector("a#open-offcanvas").addEventListener("click",()=>{
+    document.querySelector("aside#offcanvas").classList.add("show");
+});
+
+const button_close = document.querySelector("#offcanvas button.close");
+if(button_close){
+    button_close.addEventListener("click",()=>{
+        document.querySelector("aside#offcanvas").classList.remove("show");
+    });
+}
+
+document.querySelectorAll("#galeria .inner figure").forEach( (el)=>{
+    el.addEventListener("click",(event)=>{
+        const fig = el;
+        console.log(fig);
+        const image = fig.querySelector("img").src;
+        document.querySelector("dialog#lightbox img").src = image;
+        document.querySelector("dialog#lightbox").showModal();
+    })
+})
+
+
+const inputInfo = document.querySelector("#inputInfo");
+if(inputInfo){
+    inputInfo.value = navigator.userAgent;   
+}
